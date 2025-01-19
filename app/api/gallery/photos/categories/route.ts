3221386fbc610b3208c6939b49e6546ad7bde04a
Mehrdad Kohanpur <114622294+mehrdadkpur@users.noreply.github.com>
2025-01-20 01:03:18 +0330

    Update route.ts

--- app/api/gallery/photos/categories/route.ts
+++ app/api/gallery/photos/categories/route.ts
@@ -2,8 +2,6 @@ import  connectToMongodb  from '@/lib/mongodb'
 import Photo from '@/models/photo'
 import { NextResponse } from 'next/server'
 
-export const dynamic = 'force-static'
-
 export async function GET() {
     await connectToMongodb()
     const categories = await Photo.distinct('category')
