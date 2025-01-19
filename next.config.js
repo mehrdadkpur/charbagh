56fb7a51f7b19ee6cc017c8906d2140357e9258f
Mehrdad <mehrdad1990.mkp@gmail.com>
2025-01-19 21:00:10 +0330

    some update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:"standalone",
+  output: 'export',
   images: {
     unoptimized: true,
-  }
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
 }
 
-export default nextConfig;
+module.exports = nextConfig
