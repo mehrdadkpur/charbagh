c6d0978638224570b2893644b0e1b30cb826a726
Mehrdad Kohanpur <114622294+mehrdadkpur@users.noreply.github.com>
2025-01-20 01:04:30 +0330

    Update route.ts

--- app/api/gallery/videos/categories/route.ts
+++ app/api/gallery/videos/categories/route.ts
@@ -2,8 +2,6 @@ import  connectToMongodb  from '@/lib/mongodb'
 import Video from '@/models/video'
 import { NextResponse } from 'next/server'
 
-export const dynamic = 'force-static'
-
 export async function GET() {
     await connectToMongodb()
     const categories = await Video.distinct('category')
